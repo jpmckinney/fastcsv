95dccf4fbbf04de0b47b131ab3fc9d57cb0ea27e
James McKinney <james@slashpoundbang.com>
2014-11-11 14:10:09 -0500

    rewrite Ragel parser to parse valid CSVs, return [] for empty rows, raise errors if invalid, not segfault on empty files (and maybe more improvements)

--- ext/fastcsv/fastcsv.c
+++ ext/fastcsv/fastcsv.c
@@ -1,105 +1,147 @@
 
-#line 1 "ext/csvscan2/csvscan2.rl"
+#line 1 "ext/fastcsv/fastcsv.rl"
 #include <ruby.h>
+#include <ruby/encoding.h>
+// https://github.com/ruby/ruby/blob/trunk/README.EXT
+// https://github.com/ruby/ruby/blob/trunk/encoding.c
+// https://github.com/ruby/ruby/blob/master/lib/csv.rb
+// http://tools.ietf.org/html/rfc4180
+// http://w3c.github.io/csvw/syntax/#ebnf
 
-static VALUE mCSVScan, rb_eCSVScanParseError;
+static VALUE mModule, rb_eParseError;
 static ID s_read, s_to_str;
 
 
-#line 69 "ext/csvscan2/csvscan2.rl"
+#line 108 "ext/fastcsv/fastcsv.rl"
 
 
 
-#line 14 "ext/csvscan2/csvscan2.c"
-static const char _csvscan2_actions[] = {
-	0, 1, 0, 1, 4, 1, 9, 1, 
-	11, 1, 12, 1, 13, 1, 14, 2, 
-	2, 3, 2, 5, 6, 2, 5, 7, 
-	2, 5, 8, 3, 0, 1, 10, 3, 
-	9, 0, 1
+#line 20 "ext/fastcsv/fastcsv.c"
+static const char _fastcsv_actions[] = {
+	0, 1, 0, 1, 1, 1, 3, 1, 
+	6, 1, 8, 2, 3, 0, 2, 4, 
+	6, 2, 4, 8, 2, 5, 2, 2, 
+	7, 0, 3, 3, 4, 6, 3, 3, 
+	4, 8, 3, 3, 5, 2, 3, 4, 
+	7, 0, 4, 0, 3, 4, 7, 4, 
+	3, 4, 7, 0
 };
 
-static const char _csvscan2_key_offsets[] = {
-	0, 0, 2, 10, 14, 15
+static const char _fastcsv_key_offsets[] = {
+	0, 0, 3, 6, 10, 14, 18
 };
 
-static const char _csvscan2_trans_keys[] = {
-	10, 34, 9, 10, 13, 32, 34, 44, 
-	11, 12, 10, 13, 34, 44, 10, 34, 
-	0
+static const char _fastcsv_trans_keys[] = {
+	10, 13, 34, 10, 13, 34, 10, 13, 
+	34, 44, 10, 13, 34, 44, 10, 13, 
+	34, 44, 10, 13, 34, 44, 0
 };
 
-static const char _csvscan2_single_lengths[] = {
-	0, 2, 6, 4, 1, 1
+static const char _fastcsv_single_lengths[] = {
+	0, 3, 3, 4, 4, 4, 4
 };
 
-static const char _csvscan2_range_lengths[] = {
-	0, 0, 1, 0, 0, 0
+static const char _fastcsv_range_lengths[] = {
+	0, 0, 0, 0, 0, 0, 0
 };
 
-static const char _csvscan2_index_offsets[] = {
-	0, 0, 3, 11, 16, 18
+static const char _fastcsv_index_offsets[] = {
+	0, 0, 4, 8, 13, 18, 23
 };
 
-static const char _csvscan2_trans_targs[] = {
-	1, 5, 1, 2, 2, 4, 2, 1, 
-	2, 3, 3, 2, 2, 2, 2, 3, 
-	2, 2, 1, 2, 2, 2, 2, 2, 
-	0
+static const char _fastcsv_trans_targs[] = {
+	2, 2, 6, 2, 2, 2, 6, 2, 
+	3, 5, 1, 3, 4, 3, 5, 0, 
+	3, 4, 3, 5, 1, 3, 4, 3, 
+	5, 2, 3, 0, 0
 };
 
-static const char _csvscan2_trans_actions[] = {
-	1, 24, 0, 5, 31, 0, 5, 0, 
-	7, 18, 21, 13, 13, 13, 13, 21, 
-	27, 9, 0, 11, 13, 13, 9, 11, 
-	0
+static const char _fastcsv_trans_actions[] = {
+	11, 11, 34, 5, 1, 1, 20, 0, 
+	47, 47, 3, 26, 5, 38, 38, 0, 
+	14, 0, 42, 47, 3, 26, 5, 23, 
+	23, 0, 7, 0, 0
 };
 
-static const char _csvscan2_to_state_actions[] = {
-	0, 0, 15, 0, 0, 0
+static const char _fastcsv_eof_actions[] = {
+	0, 9, 9, 30, 17, 30, 9
 };
 
-static const char _csvscan2_from_state_actions[] = {
-	0, 0, 3, 0, 0, 0
-};
+static const int fastcsv_start = 3;
+static const int fastcsv_first_final = 3;
+static const int fastcsv_error = 0;
 
-static const char _csvscan2_eof_trans[] = {
-	0, 22, 0, 22, 23, 24
-};
-
-static const int csvscan2_start = 2;
-static const int csvscan2_error = 0;
-
-static const int csvscan2_en_main = 2;
+static const int fastcsv_en_main = 3;
 
 
-#line 72 "ext/csvscan2/csvscan2.rl"
+#line 111 "ext/fastcsv/fastcsv.rl"
 
 #define BUFSIZE 16384
 
-VALUE csvscan2(int argc, VALUE *argv, VALUE self)
-{
-  int cs, act, have = 0, nread = 0, curline = 1, io = 0;
-  char *ts = 0, *te = 0, *buf = NULL, *eof = NULL;
+VALUE fastcsv(int argc, VALUE *argv, VALUE self) {
+  int cs, act, have = 0, curline = 1, io = 0;
+  char *ts = 0, *te = 0, *buf = 0, *eof = 0;
 
-  VALUE port;
-  VALUE bufsize = Qnil;
-  int done = 0, buffer_size = 0, taint = 0;
+  VALUE port, opts;
+  VALUE row = rb_ary_new(), aval = Qnil, bufsize = Qnil;
+  char *mark_aval = 0;
+  int done = 0, unclosed_line = 0, buffer_size = 0, taint = 0;
+  int encoding_index = rb_enc_to_index(rb_default_external_encoding());
 
-  rb_scan_args(argc, argv, "1", &port);
+  VALUE option;
+  char quote_char = '"', *col_sep = ",", *row_sep = "\r\n";
+
+  rb_scan_args(argc, argv, "11", &port, &opts);
   taint = OBJ_TAINTED(port);
   io = rb_respond_to(port, s_read);
-  if (!io)
-  {
-    if (rb_respond_to(port, s_to_str))
-    {
+  if (!io) {
+    if (rb_respond_to(port, s_to_str)) {
       port = rb_funcall(port, s_to_str, 0);
       StringValue(port);
     }
-    else
-    {
-      rb_raise(rb_eArgError, "a CSV table must be built from an input source (a String or IO object.)");
+    else {
+      rb_raise(rb_eArgError, "data has to respond to #read or #to_str");
     }
+  }
+
+  if (NIL_P(opts)) {
+    opts = rb_hash_new();
+  }
+  else if (TYPE(opts) != T_HASH) {
+    rb_raise(rb_eArgError, "options has to be a Hash or nil");
+  }
+
+  // @todo Add machines for other common CSV formats?
+  // option = rb_hash_aref(opts, ID2SYM(rb_intern("quote_char")));
+  // if (TYPE(option) == T_STRING && RSTRING_LEN(option) == 1) {
+  //   quote_char = *StringValueCStr(option);
+  // }
+  // else if (!NIL_P(option)) {
+  //   rb_raise(rb_eArgError, ":quote_char has to be a single character String");
+  // }
+
+  // option = rb_hash_aref(opts, ID2SYM(rb_intern("col_sep")));
+  // if (TYPE(option) == T_STRING) {
+  //   col_sep = StringValueCStr(option);
+  // }
+  // else if (!NIL_P(option)) {
+  //   rb_raise(rb_eArgError, ":col_sep has to be a String");
+  // }
+
+  // option = rb_hash_aref(opts, ID2SYM(rb_intern("row_sep")));
+  // if (TYPE(option) == T_STRING) {
+  //   row_sep = StringValueCStr(option);
+  // }
+  // else if (!NIL_P(option)) {
+  //   rb_raise(rb_eArgError, ":row_sep has to be a String");
+  // }
+
+  option = rb_hash_aref(opts, ID2SYM(rb_intern("encoding")));
+  if (TYPE(option) == T_STRING) {
+    encoding_index = rb_enc_find_index(StringValueCStr(option));
+  }
+  else if (!NIL_P(option)) {
+    rb_raise(rb_eArgError, ":encoding has to be a String");
   }
 
   buffer_size = BUFSIZE;
@@ -110,34 +152,28 @@ VALUE csvscan2(int argc, VALUE *argv, VALUE self)
     }
   }
 
-  if (io)
+  if (io) {
     buf = ALLOC_N(char, buffer_size);
+  }
 
   
-#line 118 "ext/csvscan2/csvscan2.c"
+#line 161 "ext/fastcsv/fastcsv.c"
 	{
-	cs = csvscan2_start;
-	ts = 0;
-	te = 0;
-	act = 0;
+	cs = fastcsv_start;
 	}
 
-#line 112 "ext/csvscan2/csvscan2.rl"
-
-  VALUE row, coldata;
-  row = rb_ary_new();
-  coldata = Qnil;
+#line 193 "ext/fastcsv/fastcsv.rl"
 
   while (!done) {
     VALUE str;
     char *p, *pe;
-    int len, space = buffer_size - have, tokstart_diff, tokend_diff;
+    int len, space = buffer_size - have, tokstart_diff, tokend_diff, mark_aval_diff;
 
-    if (io)
-    {
+    if (io) {
       if (space == 0) {
          tokstart_diff = ts - buf;
          tokend_diff = te - buf;
+         mark_aval_diff = mark_aval - buf;
 
          buffer_size += BUFSIZE;
          REALLOC_N(buf, char, buffer_size);
@@ -146,31 +182,36 @@ VALUE csvscan2(int argc, VALUE *argv, VALUE self)
 
          ts = buf + tokstart_diff;
          te = buf + tokend_diff;
+         mark_aval = buf + mark_aval_diff;
       }
       p = buf + have;
 
       str = rb_funcall(port, s_read, 1, INT2FIX(space));
-      len = RSTRING_LEN(str);
-      memcpy(p, StringValuePtr(str), len);
+      if (NIL_P(str)) {
+        // StringIO#read returns nil for empty string.
+        len = 0;
+      }
+      else {
+        len = RSTRING_LEN(str);
+        memcpy(p, StringValuePtr(str), len);
+      }
+
+      if (len < space) {
+        done = 1;
+      }
     }
-    else
-    {
+    else {
       p = RSTRING_PTR(port);
       len = RSTRING_LEN(port) + 1;
       done = 1;
     }
 
-    nread += len;
-
-    /* If this is the last buffer, tack on an EOF. */
-    if (io && len < space) {
-      p[len++] = 0;
-      done = 1;
-    }
-
     pe = p + len;
+    if (done) {
+      eof = pe;
+    }
     
-#line 174 "ext/csvscan2/csvscan2.c"
+#line 215 "ext/fastcsv/fastcsv.c"
 	{
 	int _klen;
 	unsigned int _trans;
@@ -183,22 +224,10 @@ VALUE csvscan2(int argc, VALUE *argv, VALUE self)
 	if ( cs == 0 )
 		goto _out;
 _resume:
-	_acts = _csvscan2_actions + _csvscan2_from_state_actions[cs];
-	_nacts = (unsigned int) *_acts++;
-	while ( _nacts-- > 0 ) {
-		switch ( *_acts++ ) {
-	case 4:
-#line 1 "NONE"
-	{ts = p;}
-	break;
-#line 195 "ext/csvscan2/csvscan2.c"
-		}
-	}
+	_keys = _fastcsv_trans_keys + _fastcsv_key_offsets[cs];
+	_trans = _fastcsv_index_offsets[cs];
 
-	_keys = _csvscan2_trans_keys + _csvscan2_key_offsets[cs];
-	_trans = _csvscan2_index_offsets[cs];
-
-	_klen = _csvscan2_single_lengths[cs];
+	_klen = _fastcsv_single_lengths[cs];
 	if ( _klen > 0 ) {
 		const char *_lower = _keys;
 		const char *_mid;
@@ -221,7 +250,7 @@ _resume:
 		_trans += _klen;
 	}
 
-	_klen = _csvscan2_range_lengths[cs];
+	_klen = _fastcsv_range_lengths[cs];
 	if ( _klen > 0 ) {
 		const char *_lower = _keys;
 		const char *_mid;
@@ -244,165 +273,114 @@ _resume:
 	}
 
 _match:
-_eof_trans:
-	cs = _csvscan2_trans_targs[_trans];
+	cs = _fastcsv_trans_targs[_trans];
 
-	if ( _csvscan2_trans_actions[_trans] == 0 )
+	if ( _fastcsv_trans_actions[_trans] == 0 )
 		goto _again;
 
-	_acts = _csvscan2_actions + _csvscan2_trans_actions[_trans];
+	_acts = _fastcsv_actions + _fastcsv_trans_actions[_trans];
 	_nacts = (unsigned int) *_acts++;
 	while ( _nacts-- > 0 )
 	{
 		switch ( *_acts++ )
 		{
 	case 0:
-#line 9 "ext/csvscan2/csvscan2.rl"
+#line 15 "ext/fastcsv/fastcsv.rl"
 	{
-    curline += 1;
+    curline++;
   }
 	break;
 	case 1:
-#line 19 "ext/csvscan2/csvscan2.rl"
+#line 19 "ext/fastcsv/fastcsv.rl"
 	{
-      rb_ary_push(row, coldata);
-      rb_yield(row);
-      coldata = Qnil;
-      row = rb_ary_new();
-    }
+    unclosed_line = curline;
+  }
 	break;
-	case 5:
-#line 1 "NONE"
-	{te = p+1;}
+	case 2:
+#line 23 "ext/fastcsv/fastcsv.rl"
+	{
+    unclosed_line = 0;
+  }
 	break;
-	case 6:
-#line 18 "ext/csvscan2/csvscan2.rl"
-	{act = 1;}
-	break;
-	case 7:
-#line 29 "ext/csvscan2/csvscan2.rl"
-	{act = 4;}
-	break;
-	case 8:
-#line 48 "ext/csvscan2/csvscan2.rl"
-	{act = 5;}
-	break;
-	case 9:
-#line 18 "ext/csvscan2/csvscan2.rl"
-	{te = p+1;}
-	break;
-	case 10:
-#line 24 "ext/csvscan2/csvscan2.rl"
-	{te = p+1;}
-	break;
-	case 11:
-#line 25 "ext/csvscan2/csvscan2.rl"
-	{te = p+1;{
-      rb_ary_push(row, coldata);
-      coldata = Qnil;
-    }}
-	break;
-	case 12:
-#line 18 "ext/csvscan2/csvscan2.rl"
-	{te = p;p--;}
-	break;
-	case 13:
-#line 48 "ext/csvscan2/csvscan2.rl"
-	{te = p;p--;{
-      unsigned char ch, *start_p, *wptr, *rptr;
-      int rest, datalen;
-      start_p = wptr = ts;
-      rptr = ts + 1;
-      rest = te - ts - 2;
-      datalen = 0;
-      while(rest>0) {
-        ch = *rptr++;
-        if (ch=='"') {
-        rptr++;
-        rest--;
-        }
-        *wptr++ = ch;
-        datalen++;
-        rest--;
-      }
-      coldata = rb_str_new( start_p, datalen );
-    }}
-	break;
-	case 14:
-#line 1 "NONE"
-	{	switch( act ) {
-	case 0:
-	{{cs = 0; goto _again;}}
+	case 3:
+#line 27 "ext/fastcsv/fastcsv.rl"
+	{
+    mark_aval = p;
+  }
 	break;
 	case 4:
-	{{p = ((te))-1;}
-      unsigned char ch, *endp;
-      int datalen;
-      datalen = te - ts;
-      endp = te - 1;
-      while(datalen>0) {
-        ch = *endp--;
-        if (ch==' ' || ch=='\t') {
-          datalen--;
-        } else {
-          break;
-        }
-      }
-      if (datalen==0) {
-        coldata = Qnil;
-      } else {
-        coldata = rb_str_new(ts, datalen);
-      }
+#line 31 "ext/fastcsv/fastcsv.rl"
+	{
+    if (p == mark_aval) {
+      // Unquoted empty fields are nil, not "", in Ruby.
+      aval = Qnil;
     }
+    else if (p > mark_aval) {
+      aval = rb_str_new(mark_aval, p - mark_aval);
+      rb_enc_associate_index(aval, encoding_index);
+    }
+  }
 	break;
 	case 5:
-	{{p = ((te))-1;}
-      unsigned char ch, *start_p, *wptr, *rptr;
-      int rest, datalen;
-      start_p = wptr = ts;
-      rptr = ts + 1;
-      rest = te - ts - 2;
-      datalen = 0;
-      while(rest>0) {
-        ch = *rptr++;
-        if (ch=='"') {
-        rptr++;
-        rest--;
-        }
-        *wptr++ = ch;
-        datalen++;
-        rest--;
-      }
-      coldata = rb_str_new( start_p, datalen );
+#line 42 "ext/fastcsv/fastcsv.rl"
+	{
+    if (p == mark_aval) {
+      aval = rb_str_new2("");
+      rb_enc_associate_index(aval, encoding_index);
     }
+    else if (p > mark_aval) {
+      // Operating on mark_aval in-place produces odd behavior.
+      char *mark_copy = ALLOC_N(char, p - mark_aval);
+      memcpy(mark_copy, mark_aval, p - mark_aval);
+
+      char *reader = mark_aval, *writer = mark_copy;
+      int escaped = 0;
+
+      while (p > reader) {
+        if (*reader == quote_char && !escaped) {
+          // Skip the escaping character.
+          escaped = 1;
+        }
+        else {
+          escaped = 0;
+          *writer++ = *reader;
+        }
+        reader++;
+      }
+
+      aval = rb_str_new(mark_copy, writer - mark_copy);
+      rb_enc_associate_index(aval, encoding_index);
+
+      if (mark_copy != NULL) {
+        free(mark_copy);
+      }
+    }
+  }
 	break;
-	default:
-	{{p = ((te))-1;}}
+	case 6:
+#line 76 "ext/fastcsv/fastcsv.rl"
+	{
+    rb_ary_push(row, aval);
+    aval = Qnil;
+  }
 	break;
-	}
-	}
+	case 7:
+#line 81 "ext/fastcsv/fastcsv.rl"
+	{
+    if (!NIL_P(aval) || RARRAY_LEN(row)) { // same as new_field
+      rb_ary_push(row, aval);
+      aval = Qnil;
+    }
+
+    rb_yield(row);
+    row = rb_ary_new();
+  }
 	break;
-#line 386 "ext/csvscan2/csvscan2.c"
+#line 380 "ext/fastcsv/fastcsv.c"
 		}
 	}
 
 _again:
-	_acts = _csvscan2_actions + _csvscan2_to_state_actions[cs];
-	_nacts = (unsigned int) *_acts++;
-	while ( _nacts-- > 0 ) {
-		switch ( *_acts++ ) {
-	case 2:
-#line 1 "NONE"
-	{ts = 0;}
-	break;
-	case 3:
-#line 1 "NONE"
-	{act = 0;}
-	break;
-#line 403 "ext/csvscan2/csvscan2.c"
-		}
-	}
-
 	if ( cs == 0 )
 		goto _out;
 	if ( ++p != pe )
@@ -410,48 +388,99 @@ _again:
 	_test_eof: {}
 	if ( p == eof )
 	{
-	if ( _csvscan2_eof_trans[cs] > 0 ) {
-		_trans = _csvscan2_eof_trans[cs] - 1;
-		goto _eof_trans;
+	const char *__acts = _fastcsv_actions + _fastcsv_eof_actions[cs];
+	unsigned int __nacts = (unsigned int) *__acts++;
+	while ( __nacts-- > 0 ) {
+		switch ( *__acts++ ) {
+	case 3:
+#line 27 "ext/fastcsv/fastcsv.rl"
+	{
+    mark_aval = p;
+  }
+	break;
+	case 4:
+#line 31 "ext/fastcsv/fastcsv.rl"
+	{
+    if (p == mark_aval) {
+      // Unquoted empty fields are nil, not "", in Ruby.
+      aval = Qnil;
+    }
+    else if (p > mark_aval) {
+      aval = rb_str_new(mark_aval, p - mark_aval);
+      rb_enc_associate_index(aval, encoding_index);
+    }
+  }
+	break;
+	case 8:
+#line 102 "ext/fastcsv/fastcsv.rl"
+	{
+    if (!NIL_P(aval) || RARRAY_LEN(row)) {
+      rb_ary_push(row, aval);
+      rb_yield(row);
+    }
+  }
+	break;
+#line 424 "ext/fastcsv/fastcsv.c"
+		}
 	}
 	}
 
 	_out: {}
 	}
 
-#line 159 "ext/csvscan2/csvscan2.rl"
+#line 241 "ext/fastcsv/fastcsv.rl"
 
-    if (cs == csvscan2_error) {
-      if (buf != NULL)
+    if (cs < fastcsv_first_final) {
+      if (buf != NULL) {
         free(buf);
-      rb_raise(rb_eCSVScanParseError, "parse error on line %d.", curline);
+      }
+      if (unclosed_line) {
+        rb_raise(rb_eParseError, "Unclosed quoted field on line %d.", unclosed_line);
+      }
+      // Ruby raises different errors for illegal quoting, depending on whether
+      // a quoted string is followed by a string ("Unclosed quoted field on line
+      // %d.") or by a string ending in a quote ("Missing or stray quote in line
+      // %d"). These precisions are kind of bogus.
+      else {
+        rb_raise(rb_eParseError, "Illegal quoting in line %d.", curline);
+      }
     }
 
-    if (ts == 0)
-    {
+    // @todo
+    // EOF actions don't work in scanners.
+    // @see http://www.complang.org/pipermail/ragel-users/2007-May/001516.html
+    // if (done && (!NIL_P(aval) || RARRAY_LEN(row))) {
+    //   rb_ary_push(row, aval);
+    //   rb_yield(row);
+    // }
+
+    if (ts == 0) {
       have = 0;
     }
-    else if (io)
-    {
+    else if (io) {
       have = pe - ts;
       memmove(buf, ts, have);
+      if (mark_aval > ts) {
+        mark_aval = buf + (mark_aval - ts);
+      }
       te = buf + (te - ts);
       ts = buf;
     }
   }
 
-  if (buf != NULL)
+  if (buf != NULL) {
     free(buf);
+  }
 
   return Qnil;
 }
 
-void Init_csvscan2() {
+void Init_fastcsv() {
   s_read = rb_intern("read");
   s_to_str = rb_intern("to_str");
 
-  mCSVScan = rb_define_module("CSVScan");
-  rb_define_attr(rb_singleton_class(mCSVScan), "buffer_size", 1, 1);
-  rb_define_singleton_method(mCSVScan, "scan", csvscan2, -1);
-  rb_eCSVScanParseError = rb_define_class_under(mCSVScan, "ParseError", rb_eStandardError);
+  mModule = rb_define_module("FastCSV");
+  rb_define_attr(rb_singleton_class(mModule), "buffer_size", 1, 1);
+  rb_define_singleton_method(mModule, "scan", fastcsv, -1);
+  rb_eParseError = rb_define_class_under(mModule, "ParseError", rb_eStandardError);
 }
--- ext/fastcsv/fastcsv.rl
+++ ext/fastcsv/fastcsv.rl
@@ -1,100 +1,180 @@
 #include <ruby.h>
+#include <ruby/encoding.h>
+// https://github.com/ruby/ruby/blob/trunk/README.EXT
+// https://github.com/ruby/ruby/blob/trunk/encoding.c
+// https://github.com/ruby/ruby/blob/master/lib/csv.rb
+// http://tools.ietf.org/html/rfc4180
+// http://w3c.github.io/csvw/syntax/#ebnf
 
-static VALUE mCSVScan, rb_eCSVScanParseError;
+static VALUE mModule, rb_eParseError;
 static ID s_read, s_to_str;
 
 %%{
-  machine csvscan2;
+  machine fastcsv;
 
-  newline = ('\r\n' | '\n') @{
-    curline += 1;
-  };
-  Separator = ',';
-  UnQuotedValue = [^ \t",\r\n] [^",\r\n]*;
-  QuotedChar = ( '""' | [^"] | newline );
-  QuotedValue = '"' . QuotedChar* . '"';
+  action new_line {
+    curline++;
+  }
 
-  main := |*
-    space;
-    newline @{
-      rb_ary_push(row, coldata);
+  action open_quote {
+    unclosed_line = curline;
+  }
+
+  action close_quote {
+    unclosed_line = 0;
+  }
+
+  action mark_start {
+    mark_aval = p;
+  }
+
+  action read_unquoted {
+    if (p == mark_aval) {
+      // Unquoted empty fields are nil, not "", in Ruby.
+      aval = Qnil;
+    }
+    else if (p > mark_aval) {
+      aval = rb_str_new(mark_aval, p - mark_aval);
+      rb_enc_associate_index(aval, encoding_index);
+    }
+  }
+
+  action read_quoted {
+    if (p == mark_aval) {
+      aval = rb_str_new2("");
+      rb_enc_associate_index(aval, encoding_index);
+    }
+    else if (p > mark_aval) {
+      // Operating on mark_aval in-place produces odd behavior.
+      char *mark_copy = ALLOC_N(char, p - mark_aval);
+      memcpy(mark_copy, mark_aval, p - mark_aval);
+
+      char *reader = mark_aval, *writer = mark_copy;
+      int escaped = 0;
+
+      while (p > reader) {
+        if (*reader == quote_char && !escaped) {
+          // Skip the escaping character.
+          escaped = 1;
+        }
+        else {
+          escaped = 0;
+          *writer++ = *reader;
+        }
+        reader++;
+      }
+
+      aval = rb_str_new(mark_copy, writer - mark_copy);
+      rb_enc_associate_index(aval, encoding_index);
+
+      if (mark_copy != NULL) {
+        free(mark_copy);
+      }
+    }
+  }
+
+  action new_field {
+    rb_ary_push(row, aval);
+    aval = Qnil;
+  }
+
+  action new_row {
+    if (!NIL_P(aval) || RARRAY_LEN(row)) { // same as new_field
+      rb_ary_push(row, aval);
+      aval = Qnil;
+    }
+
+    rb_yield(row);
+    row = rb_ary_new();
+  }
+
+  quote_char = '"';
+  col_sep = ',' >new_field;
+  row_sep = ('\r' '\n'? | '\n') @new_line;
+  unquoted = (any* -- quote_char -- col_sep -- row_sep) >mark_start %read_unquoted;
+  quoted = quote_char >open_quote (^quote_char | quote_char quote_char | row_sep)* >mark_start %read_quoted quote_char >close_quote;
+  field = unquoted | quoted;
+  fields = (field col_sep)* field?;
+  file = (fields row_sep >new_row)* fields?;
+
+  # @see Ragel Guide: 6.3 Scanners
+  # @todo Restore scanner.
+  main := file $/{
+    if (!NIL_P(aval) || RARRAY_LEN(row)) {
+      rb_ary_push(row, aval);
       rb_yield(row);
-      coldata = Qnil;
-      row = rb_ary_new();
-    };
-    Separator {
-      rb_ary_push(row, coldata);
-      coldata = Qnil;
-    };
-    UnQuotedValue {
-      unsigned char ch, *endp;
-      int datalen;
-      datalen = te - ts;
-      endp = te - 1;
-      while(datalen>0) {
-        ch = *endp--;
-        if (ch==' ' || ch=='\t') {
-          datalen--;
-        } else {
-          break;
-        }
-      }
-      if (datalen==0) {
-        coldata = Qnil;
-      } else {
-        coldata = rb_str_new(ts, datalen);
-      }
-    };
-    QuotedValue {
-      unsigned char ch, *start_p, *wptr, *rptr;
-      int rest, datalen;
-      start_p = wptr = ts;
-      rptr = ts + 1;
-      rest = te - ts - 2;
-      datalen = 0;
-      while(rest>0) {
-        ch = *rptr++;
-        if (ch=='"') {
-        rptr++;
-        rest--;
-        }
-        *wptr++ = ch;
-        datalen++;
-        rest--;
-      }
-      coldata = rb_str_new( start_p, datalen );
-    };
-  *|;
-
+    }
+  };
 }%%
 
-%% write data nofinal;
+%% write data;
 
 #define BUFSIZE 16384
 
-VALUE csvscan2(int argc, VALUE *argv, VALUE self)
-{
-  int cs, act, have = 0, nread = 0, curline = 1, io = 0;
-  char *ts = 0, *te = 0, *buf = NULL, *eof = NULL;
+VALUE fastcsv(int argc, VALUE *argv, VALUE self) {
+  int cs, act, have = 0, curline = 1, io = 0;
+  char *ts = 0, *te = 0, *buf = 0, *eof = 0;
 
-  VALUE port;
-  VALUE bufsize = Qnil;
-  int done = 0, buffer_size = 0, taint = 0;
+  VALUE port, opts;
+  VALUE row = rb_ary_new(), aval = Qnil, bufsize = Qnil;
+  char *mark_aval = 0;
+  int done = 0, unclosed_line = 0, buffer_size = 0, taint = 0;
+  int encoding_index = rb_enc_to_index(rb_default_external_encoding());
 
-  rb_scan_args(argc, argv, "1", &port);
+  VALUE option;
+  char quote_char = '"', *col_sep = ",", *row_sep = "\r\n";
+
+  rb_scan_args(argc, argv, "11", &port, &opts);
   taint = OBJ_TAINTED(port);
   io = rb_respond_to(port, s_read);
-  if (!io)
-  {
-    if (rb_respond_to(port, s_to_str))
-    {
+  if (!io) {
+    if (rb_respond_to(port, s_to_str)) {
       port = rb_funcall(port, s_to_str, 0);
       StringValue(port);
     }
-    else
-    {
-      rb_raise(rb_eArgError, "a CSV table must be built from an input source (a String or IO object.)");
+    else {
+      rb_raise(rb_eArgError, "data has to respond to #read or #to_str");
     }
+  }
+
+  if (NIL_P(opts)) {
+    opts = rb_hash_new();
+  }
+  else if (TYPE(opts) != T_HASH) {
+    rb_raise(rb_eArgError, "options has to be a Hash or nil");
+  }
+
+  // @todo Add machines for other common CSV formats?
+  // option = rb_hash_aref(opts, ID2SYM(rb_intern("quote_char")));
+  // if (TYPE(option) == T_STRING && RSTRING_LEN(option) == 1) {
+  //   quote_char = *StringValueCStr(option);
+  // }
+  // else if (!NIL_P(option)) {
+  //   rb_raise(rb_eArgError, ":quote_char has to be a single character String");
+  // }
+
+  // option = rb_hash_aref(opts, ID2SYM(rb_intern("col_sep")));
+  // if (TYPE(option) == T_STRING) {
+  //   col_sep = StringValueCStr(option);
+  // }
+  // else if (!NIL_P(option)) {
+  //   rb_raise(rb_eArgError, ":col_sep has to be a String");
+  // }
+
+  // option = rb_hash_aref(opts, ID2SYM(rb_intern("row_sep")));
+  // if (TYPE(option) == T_STRING) {
+  //   row_sep = StringValueCStr(option);
+  // }
+  // else if (!NIL_P(option)) {
+  //   rb_raise(rb_eArgError, ":row_sep has to be a String");
+  // }
+
+  option = rb_hash_aref(opts, ID2SYM(rb_intern("encoding")));
+  if (TYPE(option) == T_STRING) {
+    encoding_index = rb_enc_find_index(StringValueCStr(option));
+  }
+  else if (!NIL_P(option)) {
+    rb_raise(rb_eArgError, ":encoding has to be a String");
   }
 
   buffer_size = BUFSIZE;
@@ -105,25 +185,22 @@ VALUE csvscan2(int argc, VALUE *argv, VALUE self)
     }
   }
 
-  if (io)
+  if (io) {
     buf = ALLOC_N(char, buffer_size);
+  }
 
   %% write init;
-
-  VALUE row, coldata;
-  row = rb_ary_new();
-  coldata = Qnil;
 
   while (!done) {
     VALUE str;
     char *p, *pe;
-    int len, space = buffer_size - have, tokstart_diff, tokend_diff;
+    int len, space = buffer_size - have, tokstart_diff, tokend_diff, mark_aval_diff;
 
-    if (io)
-    {
+    if (io) {
       if (space == 0) {
          tokstart_diff = ts - buf;
          tokend_diff = te - buf;
+         mark_aval_diff = mark_aval - buf;
 
          buffer_size += BUFSIZE;
          REALLOC_N(buf, char, buffer_size);
@@ -132,62 +209,87 @@ VALUE csvscan2(int argc, VALUE *argv, VALUE self)
 
          ts = buf + tokstart_diff;
          te = buf + tokend_diff;
+         mark_aval = buf + mark_aval_diff;
       }
       p = buf + have;
 
       str = rb_funcall(port, s_read, 1, INT2FIX(space));
-      len = RSTRING_LEN(str);
-      memcpy(p, StringValuePtr(str), len);
+      if (NIL_P(str)) {
+        // StringIO#read returns nil for empty string.
+        len = 0;
+      }
+      else {
+        len = RSTRING_LEN(str);
+        memcpy(p, StringValuePtr(str), len);
+      }
+
+      if (len < space) {
+        done = 1;
+      }
     }
-    else
-    {
+    else {
       p = RSTRING_PTR(port);
       len = RSTRING_LEN(port) + 1;
       done = 1;
     }
 
-    nread += len;
-
-    /* If this is the last buffer, tack on an EOF. */
-    if (io && len < space) {
-      p[len++] = 0;
-      done = 1;
-    }
-
     pe = p + len;
+    if (done) {
+      eof = pe;
+    }
     %% write exec;
 
-    if (cs == csvscan2_error) {
-      if (buf != NULL)
+    if (cs < fastcsv_first_final) {
+      if (buf != NULL) {
         free(buf);
-      rb_raise(rb_eCSVScanParseError, "parse error on line %d.", curline);
+      }
+      if (unclosed_line) {
+        rb_raise(rb_eParseError, "Unclosed quoted field on line %d.", unclosed_line);
+      }
+      // Ruby raises different errors for illegal quoting, depending on whether
+      // a quoted string is followed by a string ("Unclosed quoted field on line
+      // %d.") or by a string ending in a quote ("Missing or stray quote in line
+      // %d"). These precisions are kind of bogus.
+      else {
+        rb_raise(rb_eParseError, "Illegal quoting in line %d.", curline);
+      }
     }
 
-    if (ts == 0)
-    {
+    // @todo
+    // EOF actions don't work in scanners.
+    // @see http://www.complang.org/pipermail/ragel-users/2007-May/001516.html
+    // if (done && (!NIL_P(aval) || RARRAY_LEN(row))) {
+    //   rb_ary_push(row, aval);
+    //   rb_yield(row);
+    // }
+
+    if (ts == 0) {
       have = 0;
     }
-    else if (io)
-    {
+    else if (io) {
       have = pe - ts;
       memmove(buf, ts, have);
+      if (mark_aval > ts) {
+        mark_aval = buf + (mark_aval - ts);
+      }
       te = buf + (te - ts);
       ts = buf;
     }
   }
 
-  if (buf != NULL)
+  if (buf != NULL) {
     free(buf);
+  }
 
   return Qnil;
 }
 
-void Init_csvscan2() {
+void Init_fastcsv() {
   s_read = rb_intern("read");
   s_to_str = rb_intern("to_str");
 
-  mCSVScan = rb_define_module("CSVScan");
-  rb_define_attr(rb_singleton_class(mCSVScan), "buffer_size", 1, 1);
-  rb_define_singleton_method(mCSVScan, "scan", csvscan2, -1);
-  rb_eCSVScanParseError = rb_define_class_under(mCSVScan, "ParseError", rb_eStandardError);
+  mModule = rb_define_module("FastCSV");
+  rb_define_attr(rb_singleton_class(mModule), "buffer_size", 1, 1);
+  rb_define_singleton_method(mModule, "scan", fastcsv, -1);
+  rb_eParseError = rb_define_class_under(mModule, "ParseError", rb_eStandardError);
 }
