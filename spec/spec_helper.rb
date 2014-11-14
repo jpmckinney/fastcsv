1b3613e1d82ebf91d168a054f95c492f65e37eba
James McKinney <james@slashpoundbang.com>
2014-11-13 23:45:16 -0500

    ignore spec and test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,11 @@
 require 'coveralls'
 Coveralls.wear!
 
+SimpleCov.start do
+  add_filter '/spec/'
+  add_filter '/test/'
+end
+
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
